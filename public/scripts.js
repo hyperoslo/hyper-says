b397b01278454f98317a48bb19adea688ae6570e
Johannes Gorset <jgorset@gmail.com>
2015-10-24 12:08:26 +0200

    Ensure the same quote is not displayed consecutively

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var currentQuote = null;
+
   var randomizeColor = function() {
     var colors = [180, 163, 205, 200, 168, 357, 297, 187, 319];
     var randomColor = colors[Math.floor(Math.random()*colors.length)];
@@ -9,7 +11,11 @@ $(document).ready(function() {
   }
 
   var randomizeQuote = function() {
-    var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    do {
+      var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    } while (currentQuote && randomQuote.quote == currentQuote.quote);
+
+    currentQuote = randomQuote;
 
     $(".quote").text(randomQuote.quote);
     $(".author").text(randomQuote.author);
