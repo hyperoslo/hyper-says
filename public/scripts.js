b6dca0e7098db081105a38e33d134d2096ddf7f4
Sindre Moen <sindrenm@gmail.com>
2015-11-17 10:24:39 +0100

    Convert to vanilla JavaScript and remove jQuery

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
   var currentQuote = null;
   var currentColor = null;
 
@@ -6,30 +6,33 @@ $(document).ready(function() {
     var colors = [180, 163, 205, 200, 168, 357, 297, 187, 319];
 
     do {
-      var randomColor = colors[Math.floor(Math.random()*colors.length)];
+      var randomColor = colors[Math.floor(Math.random() * colors.length)];
     } while (currentColor && randomColor == currentColor);
 
     currentColor = randomColor;
 
-    $("body").css({
-      background: 'hsl(' + randomColor + ', 50%, 55%)'
-    });
+    var body = document.querySelector('body');
+    body.style.background = 'hsl(' + randomColor + ', 50%, 55%)';
   }
 
   var randomizeQuote = function() {
     do {
-      var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+      var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     } while (currentQuote && randomQuote.quote == currentQuote.quote);
 
     currentQuote = randomQuote;
 
-    $("blockquote").addClass("flipOutX");
+    var blockquote = document.querySelector('blockquote');
+    var quote = document.querySelector('.quote');
+    var author = document.querySelector('.author');
+
+    blockquote.classList.add("flipOutX");
 
     setTimeout(function() {
-      $(".quote").text(randomQuote.quote);
-      $(".author").text("– " + randomQuote.author);
-      $("blockquote").removeClass("flipOutX");
-      $("blockquote").addClass("flipInX");
+      quote.innerText = randomQuote.quote;
+      author.innerText = "– " + randomQuote.author;
+      blockquote.classList.remove("flipOutX");
+      blockquote.classList.add("flipInX");
     }, 1000);
   }
 
@@ -42,7 +45,7 @@ $(document).ready(function() {
 
   var interval = startInterval();
 
-  $("body, blockquote").on("click", function() {
+  var clickListener = function() {
     clearInterval(interval);
 
     // Prevent switching quote and color if event is triggered by text selection
@@ -52,12 +55,15 @@ $(document).ready(function() {
     randomizeQuote();
 
     interval = startInterval();
-  });
+  };
+
+  document.querySelector('body').addEventListener("click", clickListener);
+  document.querySelector('blockquote').addEventListener("click", clickListener);
 
   randomizeColor();
   randomizeQuote();
 
-  if(window.location.hash == "#tv") {
-    $(".links").hide();
+  if (window.location.hash == "#tv") {
+    document.querySelector(".links").style.display = 'none';
   }
 });
