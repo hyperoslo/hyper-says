8104f9d3b5b638df6f88d42d349ea54ace85f224
f <frodsan@protonmail.com>
2015-12-04 18:40:19 -0500

    Shuffle array so no single quote is repeated
    - Fixes quotes repetition.
    - Fixes Firefox bug (innerText is not standard, use textContent instead)
    - Don't change quote if text is selected.

--- public/scripts.js
+++ public/scripts.js
@@ -1,72 +1,88 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var currentQuote = null;
-  var currentColor = null;
+document.addEventListener("DOMContentLoaded", function() {
+  var querySelector = document.querySelector.bind(document);
 
-  var randomizeColor = function() {
-    var colors = [180, 163, 205, 200, 168, 357, 297, 187, 319];
-
-    do {
-      var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    } while (currentColor && randomColor == currentColor);
-
-    currentColor = randomColor;
-
-    var body = document.querySelector('body');
-    body.style.background = 'hsl(' + randomColor + ', 50%, 55%)';
+  function random(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
   }
 
-  var randomizeQuote = function() {
-    do {
-      var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    } while (currentQuote && randomQuote.quote == currentQuote.quote);
+  function shuffle(array) {
+    var index = -1;
+    var length = array.length;
+    var result = array.slice(0);
 
-    currentQuote = randomQuote;
+    while (++index < length) {
+      var rand = random(index, length - 1);
+      var value = result[rand];
 
-    var blockquote = document.querySelector('blockquote');
-    var quote = document.querySelector('.quote');
-    var author = document.querySelector('.author');
+      result[rand] = result[index];
+      result[index] = value;
+    }
+
+    result.length = length;
+
+    return result;
+  }
+
+  function rotate(array, fn) {
+    var value = array.pop();
+
+    fn(value);
+
+    array.unshift(value);
+  }
+
+  function setBackground(color) {
+    document.body.style.background = "hsl(" + color + ", 50%, 55%)";
+  }
+
+  function setQuote(quote) {
+    var author = querySelector(".author");
+    var blockquote = querySelector("blockquote");
+    var content = querySelector(".quote");
 
     blockquote.classList.add("flipOutX");
 
     setTimeout(function() {
-      quote.innerText = randomQuote.quote;
-      author.innerText = "– " + randomQuote.author;
+      author.textContent = "— " + quote.author;
+
       blockquote.classList.remove("flipOutX");
       blockquote.classList.add("flipInX");
-      
-      quote.classList.add("notation");
+
+      content.textContent = quote.quote;
+      content.classList.add("notation");
     }, 1000);
   }
 
-  var startInterval = function() {
-    return setInterval(function() {
-      randomizeColor();
-      randomizeQuote();
-    }, 10 * 1000);
-  };
+  var quotes = shuffle(window.quotes);
+  var colors = shuffle([180, 163, 205, 200, 168, 357, 297, 187, 319]);
 
-  var interval = startInterval();
-
-  var clickListener = function() {
-    clearInterval(interval);
-
+  function changeQuote() {
     // Prevent switching quote and color if event is triggered by text selection
     if (getSelection().toString()) return;
 
-    randomizeColor();
-    randomizeQuote();
+    rotate(colors, setBackground);
+    rotate(quotes, setQuote);
+  }
 
-    interval = startInterval();
+  var interval = null;
+
+  var startInterval = function() {
+    clearInterval(interval);
+
+    changeQuote();
+
+    interval = setInterval(changeQuote, 10000);
   };
 
-  document.querySelector('body').addEventListener("click", clickListener);
-  document.querySelector('blockquote').addEventListener("click", clickListener);
+  rotate(colors, setBackground);
 
-  randomizeColor();
-  
-  setTimeout(function() { randomizeQuote() }, 1000 * 3);
+  setTimeout(function() {
+    startInterval();
 
-  if(window.location.hash.toLowerCase() == "#tv") {
-    document.querySelector(".links").style.display = 'none';
+    document.body.addEventListener("click", startInterval);
+  }, 3000);
+
+  if (window.location.hash.toLowerCase() === "#tv") {
+    querySelector(".links").style.display = "none";
   }
 });
